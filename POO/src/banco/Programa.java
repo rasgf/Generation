2a7a4767c707c4b186e29bd4766fbbd15cb4b7f1
rasgf <97187500+rasgf@users.noreply.github.com>
2023-09-19 10:16:28 -0300

    Colocando try-catch

--- POO/src/banco/Programa.java
+++ POO/src/banco/Programa.java
@@ -1,6 +1,6 @@
 package banco;
-
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -10,32 +10,38 @@ public class Programa {
 		
 		List<Cliente> listaClientes = new ArrayList<>();		
 		
-		for (int i = 1; i <= 1; i++) {
-			System.out.print("Qual o ID do cliente? ");
-			int id = sc.nextInt();
-			sc.skip("\\R");
-			System.out.print("Pessoa física ou jurídica (f/j)? ");
-			char type = sc.next().charAt(0);
-			sc.skip("\\R");
-			System.out.print("Qual o nome do cliente? ");
-			String nome = sc.nextLine();
-			System.out.print("Qual a idade do cliente? ");
-			int idade = sc.nextInt();
-			sc.skip("\\R");
-			System.out.print("Qual o email do cliente? ");
-			String email = sc.nextLine();
-			System.out.print("Qual o telefone do cliente? ");
-			String numero = sc.nextLine();			
+		for (int i = 1; i <= 2; i++) {
 			
-			if (type == 'j') {
-				System.out.print("Qual o CNPJ cliente? ");
-				long cnpj = sc.nextLong();
-				listaClientes.add(new PessoaJuridica(id, nome, idade, email, numero, cnpj));
-			} else {
-				System.out.print("Qual o CPF cliente? ");
-				long cpf = sc.nextLong();
-				listaClientes.add(new PessoaFisica(id, nome, idade, email, numero, cpf));
-			}
+				try {
+					System.out.print("Qual o ID do cliente? ");
+					int id = sc.nextInt();
+					sc.skip("\\R");
+					System.out.print("Pessoa física ou jurídica (f/j)? ");
+					char type = sc.next().charAt(0);
+					sc.skip("\\R");
+					System.out.print("Qual o nome do cliente? ");
+					String nome = sc.nextLine();
+					System.out.print("Qual a idade do cliente? ");
+					int idade = sc.nextInt();
+					sc.skip("\\R");
+					System.out.print("Qual o email do cliente? ");
+					String email = sc.nextLine();
+					System.out.print("Qual o telefone do cliente? ");
+					String numero = sc.nextLine();
+					if (type == 'j'){
+						System.out.print("Qual o CNPJ cliente? ");
+						long cnpj = sc.nextLong();
+						listaClientes.add(new PessoaJuridica(id, nome, idade, email, numero, cnpj));
+					} else if (type == 'f') {
+						System.out.print("Qual o CPF cliente? ");
+						long cpf = sc.nextLong();
+						listaClientes.add(new PessoaFisica(id, nome, idade, email, numero, cpf));
+					}
+				} catch (InputMismatchException e) {
+					System.out.println();
+					System.err.println(e);
+					System.out.println("Valor digitado inválido");
+				}
 			
 		}
 		
